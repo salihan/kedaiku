257400bb877741dbd944c3c153163d6131d7771f
salihan <salihan@yahoo.com>
2021-04-14 16:28:32 +0800

    crud for kategori

--- src/kp/module/KategoriModule.java
+++ src/kp/module/KategoriModule.java
@@ -1,5 +1,7 @@
 package kp.module;
 
+import java.util.List;
+
 import kp.entity.Kategori;
 import lebah.module.LebahUserModule;
 import lebah.portal.action.Command;
@@ -11,7 +13,7 @@ public class KategoriModule extends LebahUserModule {
 	
 	@Override
 	public String start() {
-		
+		kategoriList();
 		return path + "/start.vm";
 	}
 	
@@ -26,7 +28,42 @@ public class KategoriModule extends LebahUserModule {
 		Kategori kategori = new Kategori();
 		kategori.setKat_nama(getParam("kat_nama"));
 		db.save(kategori);		
-		return path + "/start.vm?status='success'";
+		//return path + "/start.vm";
+		return kategoriList();
+	}
+	
+	@Command("kategoriList")
+	public String kategoriList() {
+		List<Kategori> kategories = db.list("select k from Kategori k order by k.kat_nama asc");
+		context.put("kategories", kategories);
+		return path + "/kategoriList.vm";
+	}
+	
+	@Command("kategoriDelete")
+	public String kategoriDelete() {
+		context.remove("delete_error");
+		Kategori kategori = db.find(Kategori.class, getParam("kat_id"));
+		try {
+			db.delete(kategori);
+		} catch ( Exception e ) {
+			context.put("delete_error", e.getMessage());
+		}
+		return kategoriList();
+	}
+	
+	@Command("kategoriEdit")
+	public String kategoriEdit() {
+		Kategori kategori = db.find(Kategori.class, getParam("kat_id"));
+		context.put("kategori", kategori);
+		return path + "/kategori_reg.vm";
+	}
+	
+	@Command("kategoriUpdate")
+	public String kategoriUpdate() {
+		Kategori kategori = db.find(Kategori.class, getParam("kat_id"));
+		kategori.setKat_nama(getParam("kat_nama"));
+		db.update(kategori);
+		return kategoriList();
 	}
 
 }
